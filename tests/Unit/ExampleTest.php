76108d469dbf89dc9a2a9343b0e820d2bcc10d5c
Hossein Beiki <ho33ein.b@gmail.com>
2026-09-02 23:41:15 +0330

    Add basic unit test for assertion

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Unixscript\IranAiChatbot\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * یک تست پایه برای اطمینان از صحت عملکرد سیستم تست.
+     */
+    public function test_basic_assertion_works()
+    {
+        $this->assertTrue(true);
+    }
+}
